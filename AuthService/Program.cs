c652a484ea3d8cee76df80d636ff4c699979038c
Andrey Solodnikov <confection12543@gmail.com>
2025-05-26 01:04:19 +0700

    Add AuthService

---
+++ AuthService/Program.cs
@@ -0,0 +1,26 @@
+using AuthService.KeycloakApiClient;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddHttpClient<KeycloakApiClient>();
+builder.Services.AddSingleton<IKeycloakApiClient, KeycloakApiClient>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
